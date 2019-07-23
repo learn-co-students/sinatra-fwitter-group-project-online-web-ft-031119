315b7d17587ccf61b65ecfaa29005dbb58ebdc21
someotherkyle <kylemclellan@gmail.com>
2019-07-23 11:02:14 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :tweets
+  has_secure_password
+
+  validates :username, presence: true
+
+  def slug
+    self.username.downcase.gsub(/\s+/,'-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |s| s.slug == slug }
+  end
+
 end
